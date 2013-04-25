ceac38ee2e5579b28cf9726973fc7de1d3f5f060
Janne Kähkönen <sugardrunk@hear.fi>
2013-04-25 22:29:24 +0300

    Comments + Syntax
    Some comments added + some syntax corrected/cleaned...

--- index.php
+++ index.php
@@ -2,8 +2,10 @@
 <html>
 
 	<title>Roomservice</title>
+	
 	<link href="css/normalize.css" rel="stylesheet" type="text/css"/>
 	<link href="css/datepicker.css" rel="stylesheet" type="text/css"/>	
+	
 	<script type="text/javascript" src="js/jquery-1.7.1.min.js"></script>
 	<script type="text/javascript" src="js/jquery-ui-1.8.18.custom.min.js"></script>
 	
@@ -14,44 +16,58 @@
 	$(function() {
 	
 		var Event = function(text, className) {
+		
 			this.text = text;
 			this.className = className;
+			
 		};
 		
-		    var list=[];
+		// A list for storing the dates of reservations.
+		var list = [];
     
-    	<?php
-      		$db=mysql_connect('<server>','<user>','<password>') or die('Error connecting to the server');
-      		mysql_select_db('<database>') or die('Error selecting database');
-      
-      		$result=mysql_query('SELECT * FROM bondit') or die ('Error performing query');
-      
-      		while($row=mysql_fetch_array($result, MYSQL_ASSOC)) {
-      
-    	?>
-    
-    	list[list.length]=<?php echo '"'.$row['date'].'"'?>;
+	   	<?php
+	    	
+	    	// Connect to database.
+	    	$db = mysql_connect('<server>','<user>','<password>') or die('Error connecting to the server');
+	      	mysql_select_db('<database>') or die('Error selecting database');
+
+			// Select from database.
+	      	$result = mysql_query('SELECT * FROM bondit') or die ('Error performing query');
+	      	
+	      	// Start fetching data from the database.
+	      	while($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
+	      			
+	    ?>
+	    
+	    list[list.length] = <?php echo '"'.$row['date'].'"'?>;	    	
+	    
+	    <?php
+	    
+	    	// End database fetching while loop here
+	      	}
+	      		
+	    ?>
     	
-    
-    	<?php
-    
-      		}       
-    	?>
-    	
+    	// DEBUG
     	alert(list.length);
     	
+    	// A list for storing the events.
     	var events = [];
     	
-    	for(var i=0; i < list.length; i++) {
-        	alert(list[i]);
+    	for(var i = 0; i < list.length; i++) {
+    	
+    		// DEBUG
+        	alert(list[i]);        	
+        	
         	events[new Date(list[i])] = new Event("Valentines Day", "pink");
         	
     	}
 
-		
+		// REPLACE THESE!
 		events[new Date("04/10/2013")] = new Event("Valentines Day", "pink");
 		events[new Date("04/12/2013")] = new Event("Payday", "green");
 
+		// DEBUG
 		console.dir(events);
 
 		$("#datepicker").datepicker({
@@ -61,30 +77,31 @@
 				var event = events[date];
 				
 				if (event) {
-					//alert(event.text);
-					e = event.text;
-					return [true, event.className, event.text];
+				
+					return [true, event.className, event.text];	
 					
 				}
-				else {
+				
+				else {	
+				
 					return [true, '', ''];
+
 				}
 			},
 			
 			onSelect: function(date) {
 				
+				// REPLACE THIS!
 				alert(date);
 			}
 		});
 		
 	});
     
-    </script>
-	
-</head>
-<head>
-</head>
+    </script>	
+
 <body>
 	<div id="datepicker"></div>
 </body>
+
 </html>
