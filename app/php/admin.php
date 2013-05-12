7990adc2e5a01339c4d9ea31cc8dd4203cd1b490
Janne Kähkönen <sugardrunk@hear.fi>
2013-05-12 13:34:34 +0300

    rooms added to admin panel.

--- app/php/admin.php
+++ app/php/admin.php
@@ -9,6 +9,9 @@
 
     $sql = "SELECT * FROM users";
     $result = mysql_query($sql);
+    
+    $sql2 = "SELECT * FROM room";
+    $result2 = mysql_query($sql2);
 
 ?>
 
@@ -29,22 +32,24 @@
 </head>
 
 <body>
-
-    <table width="400" border="0" cellspacing="1" cellpadding="0">
+       
+    <table border="0" cellspacing="1" cellpadding="0">
         <tr>
             <td>
                 <form name="form1" method="post" action="">
-                <table width="400" border="8" cellpadding="3" cellspacing="4" bgcolor="#000">
+                <table border="8" cellpadding="3" cellspacing="4" bgcolor="#000">
                 <tr>
                     <td bgcolor="#AAA">&nbsp;</td>
-                    <td colspan="4" bgcolor="#AAA"><strong><h2>Käyttäjät</h2></strong> </td>
+                    <td colspan="5" bgcolor="#AAA"><strong><h2>Käyttäjät</h2></strong> </td>
                 </tr>
                 <tr>
-                    <td align="center" bgcolor="#BBB">#</td>
-                    <td align="center" bgcolor="#BBB"><strong>Id</strong></td>
-                    <td align="center" bgcolor="#BBB"><strong>Nimi</strong></td>
-                    <td align="center" bgcolor="#BBB"><strong>Email</strong></td>
-                    <td align="center" bgcolor="#BBB"><strong>Salasana</strong></td>
+                    <td width="40" align="center" bgcolor="#BBB">#</td>
+                    <td bgcolor="#BBB"><strong>Id</strong></td>
+                    <td bgcolor="#BBB"><strong>Käyttäjätaso</strong></td>
+                    <td bgcolor="#BBB"><strong>Nimi</strong></td>
+                    <td bgcolor="#BBB"><strong>Email</strong></td>
+                    <td bgcolor="#BBB"><strong>Salasana (SHA-1)</strong></td>
+                    
                 </tr>
 
                 <?php while ($rows = mysql_fetch_array($result)): ?>
@@ -55,9 +60,65 @@
                     </td>
 
                     <td bgcolor="#FFF"><?php echo $rows['ID']; ?></td>
+                    <td bgcolor="#FFF"><?php echo htmlspecialchars($rows['userlevel']); ?></td>
                     <td bgcolor="#FFF"><?php echo htmlspecialchars($rows['username']); ?></td>
                     <td bgcolor="#FFF"><?php echo htmlspecialchars($rows['email']); ?></td>
-                    <td bgcolor="#FFF"><?php echo htmlspecialchars($rows['password']); ?></td>
+                    <td bgcolor="#FFF"><?php echo htmlspecialchars($rows['password']); ?></td>                    
+
+                </tr>
+
+                <?php endwhile; ?>
+
+                <tr>
+                    <td colspan="6" align="left" bgcolor="#555">
+                    <input name="delete" type="submit" id="delete" value="Poista"></td>
+                </tr>
+
+                <?php
+
+                    // Check if delete button is active
+                    if (!empty($_POST['delete'])) {
+
+                        foreach ($_POST['need_delete'] as $id => $value) {
+
+                            $sql = "DELETE FROM users WHERE ID='$value'";
+                            mysql_query($sql);
+                        }
+
+                        echo '<meta http-equiv="refresh" content="0;URL=admin.php">';
+                    }
+                ?>
+
+                </table>
+                </form>
+            </td>
+        </tr>
+    </table>
+    
+    <table border="0" cellspacing="1" cellpadding="0">
+        <tr>
+            <td>
+                <form name="form1" method="post" action="">
+                <table border="8" cellpadding="3" cellspacing="4" bgcolor="#000">
+                <tr>
+                    <td bgcolor="#AAA">&nbsp;</td>
+                    <td colspan="4" bgcolor="#AAA"><strong><h2>Huoneet</h2></strong> </td>
+                </tr>
+                <tr>
+                    <td width="40" align="center" bgcolor="#BBB">#</td>
+                    <td colspan="3" bgcolor="#BBB"><strong>Id</strong></td>
+                    <td bgcolor="#BBB"><strong>Huoneen tunnus</strong></td>
+                </tr>
+
+                <?php while ($rows = mysql_fetch_array($result2)): ?>
+
+                <tr>
+                    <td align="center" bgcolor="#FFFFFF">
+                    <input name="need_delete[<?php echo $rows['ID']; ?>]" type="checkbox" id="checkbox[<?php echo $rows['ID']; ?>]" value="<?php echo $rows['ID']; ?>">
+                    </td>
+
+                    <td colspan="3" bgcolor="#FFF"><?php echo $rows['ID']; ?></td>
+                    <td bgcolor="#FFF"><?php echo $rows['room_ID']; ?></td>
 
                 </tr>
 
@@ -75,8 +136,8 @@
 
                         foreach ($_POST['need_delete'] as $id => $value) {
 
-                            $sql = "DELETE FROM users WHERE ID='$value'";
-                            mysql_query($sql);
+                            $sql2 = "DELETE FROM room WHERE ID='$value'";
+                            mysql_query($sql2);
                         }
 
                         echo '<meta http-equiv="refresh" content="0;URL=admin.php">';
