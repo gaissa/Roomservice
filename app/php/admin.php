eb1244525386b1d5c6c3cd5ec59ffc6a8edb43f8
Janne Kähkönen <sugardrunk@hear.fi>
2013-05-12 12:49:53 +0300

    admin page added.

---
+++ app/php/admin.php
@@ -0,0 +1,96 @@
+<?php
+
+    // Database config
+    require_once('../conf/config.php');
+
+    // Connect to server and select databse.
+    mysql_connect("$db_host", "$db_user", "$db_pass")or die("cannot connect");
+    mysql_select_db("$db_name")or die("cannot select DB");
+
+    $sql = "SELECT * FROM users";
+    $result = mysql_query($sql);
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+
+<head>
+
+    <title>Roomservice</title>
+
+    <meta charset="utf-8">
+
+    <link href="../css/normalize.css" rel="stylesheet" type="text/css"/>
+    <link href="../css/datepicker.css" rel="stylesheet" type="text/css"/>
+    <link href="../css/adminstyle.css" rel="stylesheet" type="text/css"/>
+
+</head>
+
+<body>
+
+    <table width="400" border="0" cellspacing="1" cellpadding="0">
+        <tr>
+            <td>
+                <form name="form1" method="post" action="">
+                <table width="400" border="8" cellpadding="3" cellspacing="4" bgcolor="#000">
+                <tr>
+                    <td bgcolor="#AAA">&nbsp;</td>
+                    <td colspan="4" bgcolor="#AAA"><strong><h2>Käyttäjät</h2></strong> </td>
+                </tr>
+                <tr>
+                    <td align="center" bgcolor="#BBB">#</td>
+                    <td align="center" bgcolor="#BBB"><strong>Id</strong></td>
+                    <td align="center" bgcolor="#BBB"><strong>Nimi</strong></td>
+                    <td align="center" bgcolor="#BBB"><strong>Email</strong></td>
+                    <td align="center" bgcolor="#BBB"><strong>Salasana</strong></td>
+                </tr>
+
+                <?php while ($rows = mysql_fetch_array($result)): ?>
+
+                <tr>
+                    <td align="center" bgcolor="#FFFFFF">
+                    <input name="need_delete[<?php echo $rows['ID']; ?>]" type="checkbox" id="checkbox[<?php echo $rows['ID']; ?>]" value="<?php echo $rows['ID']; ?>">
+                    </td>
+
+                    <td bgcolor="#FFF"><?php echo $rows['ID']; ?></td>
+                    <td bgcolor="#FFF"><?php echo htmlspecialchars($rows['username']); ?></td>
+                    <td bgcolor="#FFF"><?php echo htmlspecialchars($rows['email']); ?></td>
+                    <td bgcolor="#FFF"><?php echo htmlspecialchars($rows['password']); ?></td>
+
+                </tr>
+
+                <?php endwhile; ?>
+
+                <tr>
+                    <td colspan="5" align="left" bgcolor="#555">
+                    <input name="delete" type="submit" id="delete" value="Poista"></td>
+                </tr>
+
+                <?php
+
+                    // Check if delete button is active
+                    if (!empty($_POST['delete'])) {
+
+                        foreach ($_POST['need_delete'] as $id => $value) {
+
+                            $sql = "DELETE FROM users WHERE ID='$value'";
+                            mysql_query($sql);
+                        }
+
+                        echo '<meta http-equiv="refresh" content="0;URL=admin.php">';
+                    }
+
+                    mysql_close();
+                ?>
+
+                </table>
+                </form>
+            </td>
+        </tr>
+    </table>
+
+</body>
+
+</html>
