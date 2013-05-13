f506ad72533e06bc12fe857468737a85bcb5c6bd
Janne Kähkönen <sugardrunk@hear.fi>
2013-05-13 12:30:51 +0300

    primitive input check...
    primitive input checking when adding a new user from the admin page.

--- app/php/add_admin.php
+++ app/php/add_admin.php
@@ -1,31 +1,60 @@
-<?php 
+<?php
 
     $con=mysqli_connect("localhost","root","","roomservice");
-    
+
     // Check connection
     if (mysqli_connect_errno())
     {
       echo "Failed to connect to MySQL: " . mysqli_connect_error();
     }
-      
-    $id=$_POST['id']; 
-    $taso=$_POST['taso']; 
+
+    $id=$_POST['id'];
+    $taso=$_POST['taso'];
     $nimi=$_POST['nimi'];
     $email=$_POST['email'];
-    $salasana=$_POST['salasana']; 
+    $salasana=$_POST['salasana'];
 
-    $sql="INSERT INTO users (ID, username, password, email, userlevel) VALUES ('$id','$nimi','$salasana','$email',$taso)";
+    $check = checkData($id, $taso, $nimi, $email, $salasana);
 
-    if (!mysqli_query($con,$sql))
-    {
-      die('Error: ' . mysqli_error($con));
+    if ($check) {
+
+        $sql="INSERT INTO users (ID, username, password, email, userlevel) VALUES ('$id','$nimi','$salasana','$email',$taso)";
+
+        if (!mysqli_query($con,$sql))
+        {
+          die('Error: ' . mysqli_error($con));
+        }
+
+        echo "1 record added";
+
+        mysqli_close($con);
+
+        $url = 'admin.php';
+        echo '<META HTTP-EQUIV=Refresh CONTENT="0; URL='.$url.'">';
     }
-    
-    echo "1 record added";
 
-    mysqli_close($con);
-    
-    $url = 'admin.php';
-    echo '<META HTTP-EQUIV=Refresh CONTENT="0; URL='.$url.'">';        
-     
-?> 
+    else {
+
+        $url = 'admin.php';
+        echo '<META HTTP-EQUIV=Refresh CONTENT="0; URL='.$url.'">';
+    }
+
+    # validate input
+    function checkData($id, $taso, $nimi, $email, $salasana) {
+
+        if ( ($id != '') && ($taso != '') && ($nimi != '') && ($email != '') && ($salasana != '') &&
+        ( (is_numeric($id)) && (!strstr($id, '.')) ) &&
+        ( (is_numeric($taso)) && (!strstr($taso, '.')) ) ) {
+
+            $validated = true;
+        }
+
+        else {
+
+            $validated = false;
+        }
+
+        return $validated;
+    }
+
+?>
