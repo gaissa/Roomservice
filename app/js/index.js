f753c89c74d995cdf46814b32344158d0cf82261
Janne Kähkönen <sugardrunk@hear.fi>
2013-05-14 11:31:50 +0300

    Reservation tooltips changed and alert removed.

--- app/js/index.js
+++ app/js/index.js
@@ -150,7 +150,7 @@ var isReserved;
             console.log("redarray" + m);
             var mewdate = m[2] + "/" + m[1] + "/" + m[0];
             console.log(mewdate);
-            eventss[new Date(mewdate)] = new Event("asd", "eventcolor2");
+            eventss[new Date(mewdate)] = new Event("Varattu", "eventcolor2");
         }
 
        for(var i = 0; i < rArray.length; i++) {
@@ -158,7 +158,7 @@ var isReserved;
 
             var newdate = n[2] + "/" + n[1] + "/" + n[0];
             console.log(newdate);
-            eventss[new Date(newdate)] = new Event("Varattu", "eventcolor1");
+            eventss[new Date(newdate)] = new Event("Oma varaus", "eventcolor1");
 
         }
 
@@ -293,7 +293,7 @@ var isReserved;
             // If someone else has reservations to that room, show ALERT
             } else if(data.isreserved === true && data.resuserid != userID) {
 
-                alert("PVM ON VARATTU!");
+                //alert("PVM ON VARATTU!");
 
             // If date has no reservations, show ADD button and DIALOG
             } else {
